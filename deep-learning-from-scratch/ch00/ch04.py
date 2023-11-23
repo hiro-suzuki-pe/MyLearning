8c88e8367809ef6d35fd1135a21a5408ebf0ae7f
hiro-suzuki-pe <suzuki.hiroyuki@mbr.nifty.com>
2023-11-23 13:27:41 +0900

    収束するようになった。

--- deep-learning-from-scratch/ch00/ch04.py
+++ deep-learning-from-scratch/ch00/ch04.py
@@ -1,12 +1,19 @@
 import numpy as np
-import sys, os
+import pickle
+import sys, os, time
 sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
 from common.functions import softmax
 from common.gradient import numerical_gradient
+#from two_layer_net import TwoLayerNet
+import matplotlib.pylab as plt
 
 def sigmoid(x):
     return 1.0 / (1 + np.exp(-x))
+
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+    
 
 def mean_squared_error(y, t):
     return 0.5 * np.sum((y-t)**2)
@@ -103,10 +110,87 @@ class TwoLayerNet:
 
         return grads
     
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+        
+        batch_num = x.shape[0]
+        
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+        
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+        
+        dz1 = np.dot(dy, W2.T)
+        da1 = sigmoid_grad(a1) * dz1
+        grads['W1'] = np.dot(x.T, da1)
+        grads['b1'] = np.sum(da1, axis=0)
+
+        return grads
 
 
 #---------------------------------------------------
-def main():
+def main8():     # ミニバッチ学習の実装 (結果の確認）
+    with open('train_loss_list.pkl', 'rb') as f:
+        train_loss_list = pickle.load(f)
+#    x = np.arange(len(train_loss_list))
+    x = np.arange(1000)
+    plt.plot(x, train_loss_list[:1000])
+    plt.plot(x, train_loss_list[9000:])
+    plt.ylim(0., 10.0)
+    plt.show()
+
+
+def main():     # ミニバッチ学習の実装
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+    train_loss_list = []
+
+    # Hyper parameters
+    iters_num = 10000
+    train_size = x_train.shape[0]
+    batch_size = 100
+    learning_rate = 0.1
+
+    network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+   
+    for i in range(iters_num):
+        if i % 10 == 0: pass
+    #        print('i:', i)
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+    
+        # 勾配の計算
+        # grad = network.numerical_gradient(x_batch, t_batch)
+        grad = network.gradient(x_batch, t_batch)
+
+        # Parameters update
+        for key in ('W1', 'b1', 'W2', 'b2'):
+            network.params[key] -= learning_rate * grad[key]
+        
+        # 学習経過の記録
+        loss = network.loss(x_batch, t_batch)
+        train_loss_list.append(loss)
+    
+#    print('train_loss_list:', train_loss_list)
+    with open('train_loss_list.pkl', 'wb') as f:
+        pickle.dump(train_loss_list,f)
+
+    x = np.arange(1000)
+    plt.plot(x, train_loss_list[:1000])
+    plt.plot(x, train_loss_list[9000:])
+    plt.ylim(0., 10.)
+    plt.show()
+
+
+def main6():    # 2層ニューラルネットワークのクラス
     net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
 #    net = TwoLayerNet(784, 100, 10)
     print(net.params['W1'].shape)
@@ -145,7 +229,6 @@ def main4():
 
 def main3():
 
-
     print('numerical_diff(sin,0):', numerical_diff(np.sin, 0))
     print('numerical_diff(cos,0):', numerical_diff(np.cos, 0))
 
@@ -179,4 +262,10 @@ def main1():
 
 #---------------------------------------------------
 if __name__ == '__main__':
+    start_time = time.time()
+    print('Start:', start_time)
     main()
+    end_time = time.time()
+    print('Start: {:.0f} (sec)'.format(start_time))
+    print('End:: {:.0f} (sec)'.format(end_time))
+    print('E-S: {:.2f} (sec)'.format(end_time-start_time))
